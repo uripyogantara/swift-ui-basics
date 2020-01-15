9a7fbfdfbdf88a7afb20d876360d832b175bf5d1
Urip Yogantara <uripyogantara@gmail.com>
2020-01-15 12:29:16 +0700

    add form login

--- SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/ContentView.swift
@@ -10,7 +10,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Login Account")
+                .font(.title)
+                .fontWeight(.thin)
+                .multilineTextAlignment(.leading)
+                .padding(.bottom)
+            TextField("Email", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding(.horizontal) .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding(.horizontal) .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text("Forgot Password ?")
+                .font(.caption)
+                .fontWeight(.medium)
+                .multilineTextAlignment(.trailing)
+                .foregroundColor(Color("DarkBlue"))
+                .frame(maxWidth:.infinity, alignment: .trailing)
+                .padding(.trailing)
+                
+            
+            Button(action:{
+                
+            }){
+                Text("Login")
+            }
+            .frame(minWidth:0, maxWidth: .infinity)
+            .padding(.vertical,12)
+            .background(LinearGradient(gradient: Gradient(colors: [Color("DarkBlue"), Color("LightBlue")]),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing))
+            .accentColor(Color.white)
+            .cornerRadius(8.0)
+            .padding(.horizontal)
+            .padding(.top,32.0)
+            
+            HStack{
+                Text("Don't have an Account?")
+                    .font(.caption)
+                    .fontWeight(.thin)
+                
+                Text("Sign Up")
+                    .font(.caption)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color("DarkBlue"))
+                
+            }.padding(.top,8)
+        }
     }
 }
 
